10f1525395aa3aaa698ddd3aab3ba7449114e25d
adarshshivan <adarshshivan30@gmail.com>
2025-11-03 20:32:41 +0530

    Made changes to read and update new data

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,20 @@
-fetch("data/mock_data.json")
+fetch("data/real_dashboard_data.json")
   .then(res => res.json())
   .then(data => {
     document.getElementById("content").innerHTML = `
-      <h2>EC2 Instances: ${data.EC2_Instances.length}</h2>
-      <h2>S3 Buckets: ${data.S3_Buckets.length}</h2>
+      <h2>EC2 Instances: ${data.ec2.length}</h2>
+      <ul>
+        ${data.ec2.map(i => `
+          <li>ID: ${i.InstanceId} | Type: ${i.InstanceType} | State: ${i.State} | IP: ${i.PublicIP}</li>
+        `).join('')}
+      </ul>
+
+      <h2>S3 Buckets: ${data.s3.length}</h2>
+      <ul>
+        ${data.s3.map(b => `
+          <li>Name: ${b.BucketName} | Region: ${b.Region}</li>
+        `).join('')}
+      </ul>
     `;
-  });
+  })
+  .catch(err => console.error("Error loading data:", err));
